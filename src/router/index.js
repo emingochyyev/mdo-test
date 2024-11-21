583650027515ea32568f1a4bc6d6fd9a08ca7b34
eming.g <emingochyyev@gmail.com>
2024-11-22 00:37:29 +0500

    deleted unnecessary component

--- src/router/index.js
+++ src/router/index.js
@@ -32,12 +32,11 @@ router.beforeEach((to, from, next) => {
     const token = localStorage.getItem("authToken");
 
     if (to.meta.requiresAuth && !token) {
-        // Only redirect to /login if not already on the login page
         if (to.path !== "/login") {
-            return next("/login"); // Use next instead of router.push to avoid looping
+            return next("/login");
         }
     }
-    next(); // Allow navigation if token exists or route doesn't require auth
+    next();
 });
 
 export default router;
