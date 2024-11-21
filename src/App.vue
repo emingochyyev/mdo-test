b57e8c1d48f49cd1c9df2211d54ca8e0cdd06f43
eming.g <emingochyyev@gmail.com>
2024-11-22 00:38:06 +0500

    minor style changes

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
     <div id="app">
-        <!-- <nav> -->
-        <!-- <router-link to="/">Home</router-link> |
-            <router-link to="/login">About</router-link> -->
-        <!-- </nav> -->
         <router-view />
     </div>
 </template>
@@ -17,24 +13,4 @@
     font-family: "Inter";
     src: url("./assets/Inter-Regular.ttf") format("truetype");
 }
-/* #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-} */
-/* 
-nav {
-    padding: 30px;
-} */
-/* 
-nav a {
-    font-weight: bold;
-    color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-    color: #42b983;
-} */
 </style>
